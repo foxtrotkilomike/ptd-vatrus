0871204a0dd18b0f3e750dc1373637135c51efc1
Philipp Khromov <filooloog@mail.ru>
2023-04-19 18:53:11 +0300

    fix: change theme_myboost -> theme_boost

--- frontpage/moodle/frontpage.php
+++ frontpage/moodle/frontpage.php
@@ -17,7 +17,7 @@
 /**
  * A drawer based layout for the boost theme.
  *
- * @package   theme_myboost
+ * @package   theme_boost
  * @copyright 2021 Bas Brands
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -115,4 +115,4 @@ $templatecontext = [
     'imgvatruslogofooter' => $OUTPUT->image_url('assets/vatrus-logo', 'theme'),
 ];
 
-echo $OUTPUT->render_from_template('theme_myboost/frontpage', $templatecontext);
+echo $OUTPUT->render_from_template('theme_boost/frontpage', $templatecontext);
